53a8735cb82f47635c52206fa854ec8d18e683e5
Cristian Guaman <guamanc410@gmail.com>
2026-06-12 18:08:29 +0000

    Feat:

---
+++ Unidad 02/00Javascript/string.js
@@ -0,0 +1,15 @@
+let string1 = 'Hola,  Mundo'
+let string2 = "Javascript es chevre"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es Genial'
+console.log(frase.length)
+console.log(frase.toLocaleUpperCase())
+console.log(frase.toLowerCase())
+console.log(frase.substring(0,10))
---
+++ Unidad 02/00Javascript/tipos-datos.js
@@ -0,0 +1,38 @@
+const { useInsertionEffect } = require("react")
+
+//Primitivos
+let nombre = 'Maria'
+
+//numerico
+let edad = 25
+
+//boleanos
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol valor unico
+let simboloUnico = Sumbol('único')
+
+//datos gigantes bigint
+let numeroGrande = 2n
+
+//COMPLETOS
+//object
+let carro ={
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//arrays
+let frutas = ['fresa',' mandarina',' pera',' uva',' manzana,']
+
+//functions
+function saludar(){}
+
+
+
