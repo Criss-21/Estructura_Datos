2e5344032946bc20776c9bb935982cbca39261d0
Ego_Code <criss.2004.guaman@gmail.com>
2026-06-16 21:21:56 -0500

    Feat: Se crea index.html y se implementan clases con condicionales en JS

--- Unidad 02/00Javascript/clase-condicional.js
+++ Unidad 02/00Javascript/clase-condicional.js
@@ -1,4 +1,36 @@
 /*
 condicionales
-
+if / else
 */
+let edad = 18;
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
+
+//bucle for
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+ //bucle while
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
+
+//bucle switch
+let dia = "Lunes";
+switch (dia) {
+    case "Lunes":
+        console.log("Hoy es lunes");
+        break;
+    case "Martes":
+        console.log("Hoy es martes");
+        break;
+    default:
+        console.log("No es lunes ni martes");
+        break;
+}
